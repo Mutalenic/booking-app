77faf37bcf392f9c00215346652215857b8ad414
mutalenic <nicomutale@gmail.com>
2023-09-22 15:49:52 +0200

    Add home header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,50 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+      title: "Booking.com",
+    });
+  }, [navigation]);
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Booking.com</Text>
+      </View>
+      <View style={styles.content}>
+        <Text>Home</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  header: {
+    height: 110,
+    backgroundColor: '#007AFF', // Blue background color
+    justifyContent: 'center', // Vertically center the text
+    alignItems: 'center', // Horizontally center the text
+  },
+  headerText: {
+    color: 'white', // White text color
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  content: {
+    flex: 1, // Take remaining vertical space
+    justifyContent: 'center', // Center content vertically
+    alignItems: 'center', // Center content horizontally
+  },
+});
 
-const styles = StyleSheet.create({})
+export default HomeScreen;
