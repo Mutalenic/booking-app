554f802ab50e66d3e754e270cba14e76dddb1b33
mutalenic <nicomutale@gmail.com>
2023-10-16 13:41:00 +0200

    Update date picker, rooms selector, search

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,124 +1,332 @@
-import { StyleSheet, View, ScrollView, Pressable } from 'react-native';
-import React, { useLayoutEffect } from 'react';
-import { useNavigation } from '@react-navigation/native';
-import { Ionicons } from 'react-native-vector-icons';
-import Header from '../components/Header';
+import {
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  Image,
+  Alert
+} from "react-native";
+import React, { useLayoutEffect, useState } from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import Header from "../components/Header";
+import { Feather } from "@expo/vector-icons";
+import DatePicker from "react-native-date-ranges";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const [selectedDates, setSelectedDates] = useState();
+  const route = useRoute();
+  const [rooms, setRooms] = useState(1);
+  const [adults, setAdults] = useState(2);
+  const [children, setChildren] = useState(0);
+  const [modalVisibile, setModalVisibile] = useState(false);
 
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: true,
       title: "Booking.com",
-      headerTitleAlign: 'center', // Center-align the header title
       headerTitleStyle: {
-        color: 'white', // Change the text color to white
-        fontSize: 24, // Adjust font size if needed
-        fontWeight: 'bold', // Add fontWeight if needed
-        borderBottomWidth: 0, // Make the bottom border invisible
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "white",
       },
       headerStyle: {
-        backgroundColor: '#007AFF', // Blue background color
-        borderBottomColor: 'transparent', // Set bottom border color to transparent
-        shadowColor: 'transparent', // Set shadow color to transparent
+        backgroundColor: "#003580",
+        height: 110,
+        borderBottomColor: "transparent",
+        shadowColor: "transparent",
       },
       headerRight: () => (
-        <View style={styles.notificationIcon}>
-          <Ionicons name="notifications" color="white" size={24} />
-        </View>
+        <Ionicons
+          name="notifications-outline"
+          size={24}
+          color="white"
+          style={{ marginRight: 12 }}
+        />
       ),
     });
-  }, [navigation]);
+  }, []);
+  const customButton = (onConfirm) => {
+    return (
+      <Button
+        onPress={onConfirm}
+        style={{
+          container: { width: "80%", marginHorizontal: "3%" },
+          text: { fontSize: 20 },
+        }}
+        primary
+        title="Submit"
+      />
+    );
+  };
+  console.log(route.params);
+
+  const searchPlaces = (place) => {
+    if(!route.params || !selectedDates){
+      Alert.alert(
+        "Invalid Details",
+        "Please enter all the details",
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel"
+          },
+          { text: "OK", onPress: () => console.log("OK Pressed") }
+        ],
+        { cancelable: false }
+      );
+    }
+
+    if(route.params && selectedDates){
+      navigation.navigate("Places",{
+        rooms:rooms,
+        adults:adults,
+        children:children,
+        selectedDates:selectedDates,
+        place:place
+      })
+    }
+  };
 
   return (
-    <View style={styles.container}>
-      <Header />
-      <ScrollView>
-        <View style={styles.mainRectangle}>
-          {/* Destination */}
-          <Pressable
-            style={({ pressed }) => [
-              styles.pressable,
-              {
-                borderColor: pressed ? 'transparent' : '#FFC72C',
-                marginTop: 10, // Add space above the first rectangle
-                paddingHorizontal: 10, // Add horizontal padding
-                width: '90%', // Cover 90% of the width
-              },
-            ]}
-            onPress={() => {
-              // Handle destination press
-            }}
-          >
-            <Ionicons name="search" size={24} color="#007AFF" />
-            {/* You can add text or other content here */}
-          </Pressable>
+    <>
+      <View>
+        <Header />
 
-          {/* Selected dates */}
-          <Pressable
-            style={({ pressed }) => [
-              styles.pressable,
-              {
-                borderColor: pressed ? 'transparent' : '#FFC72C',
-                paddingHorizontal: 10, // Add horizontal padding
-                width: '90%', // Cover 90% of the width
-              },
-            ]}
-            onPress={() => {
-              // Handle selected dates press
+        <ScrollView>
+          <View
+            style={{
+              margin: 20,
+              borderColor: "#FFC72C",
+              borderWidth: 3,
+              borderRadius: 6,
             }}
           >
-            <Ionicons name="calendar" size={24} color="#007AFF" />
-            {/* You can add text or other content here */}
-          </Pressable>
+            {/* Destination */}
+            <Pressable
+              onPress={() => navigation.navigate("Search")}
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+                paddingHorizontal: 10,
+                borderColor: "#FFC72C",
+                borderWidth: 2,
+                paddingVertical: 15,
+              }}
+            >
+              <Feather name="search" size={24} color="black" />
+              <TextInput
+                placeholderTextColor="black"
+                placeholder={
+                  route?.params ? route.params.input : "Enter Your Destination"
+                }
+              />
+            </Pressable>
 
-          {/* Rooms and guests */}
+            {/* Selected Dates */}
+            <Pressable
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+                paddingHorizontal: 10,
+                borderColor: "#FFC72C",
+                borderWidth: 2,
+                paddingVertical: 15,
+              }}
+            >
+              <Feather name="calendar" size={24} color="black" />
+              <DatePicker
+                style={{
+                  width: 350,
+                  height: 30,
+                  borderRadius: 0,
+                  borderWidth: 0,
+                  borderColor: "transparent",
+                }}
+                customStyles={{
+                  placeholderText: {
+                    fontSize: 15,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    marginRight: "auto",
+                  },
+                  headerStyle: {
+                    backgroundColor: "#003580",
+                  },
+                  contentText: {
+                    fontSize: 15,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    marginRight: "auto",
+                  },
+                }}
+                selectedBgColor="#0047AB"
+                customButton={(onConfirm) => customButton(onConfirm)}
+                onConfirm={(startDate, endDate) =>
+                  setSelectedDates(startDate, endDate)
+                }
+                allowFontScaling={false}
+                placeholder={"Select Your Dates"}
+                mode={"range"}
+              />
+            </Pressable>
+
+            {/* Rooms and Guests */}
+            <Pressable
+              onPress={() => setModalVisibile(!modalVisibile)}
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+                paddingHorizontal: 10,
+                borderColor: "#FFC72C",
+                borderWidth: 2,
+                paddingVertical: 15,
+              }}
+            >
+              <Ionicons name="person-outline" size={24} color="black" />
+              <TextInput
+                placeholderTextColor="red"
+                placeholder={` ${rooms} room • ${adults} adults • ${children} Children`}
+              />
+            </Pressable>
+
+            {/* Search Button */}
+            <Pressable
+              onPress={() => searchPlaces(route?.params.input)}
+              style={{
+                paddingHorizontal: 10,
+                borderColor: "#FFC72C",
+                borderWidth: 2,
+                paddingVertical: 15,
+                backgroundColor: "#2a52be",
+              }}
+            >
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontSize: 15,
+                  fontWeight: "500",
+                  color: "white",
+                }}
+              >
+                Search
+              </Text>
+            </Pressable>
+          </View>
+
+          <Text
+            style={{ marginHorizontal: 20, fontSize: 17, fontWeight: "500" }}
+          >
+            Travel More spend less
+          </Text>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            <Pressable
+              style={{
+                width: 200,
+                height: 150,
+                marginTop: 10,
+                backgroundColor: "#003580",
+                borderRadius: 10,
+                padding: 20,
+                marginHorizontal: 20,
+              }}
+            >
+              <Text
+                style={{
+                  color: "white",
+                  fontSize: 15,
+                  fontWeight: "bold",
+                  marginVertical: 7,
+                }}
+              >
+                Genius
+              </Text>
+              <Text style={{ color: "white", fontSize: 15, fontWeight: "500" }}>
+                You are ate genius level one in our loyalty program
+              </Text>
+            </Pressable>
+
+            <Pressable
+              style={{
+                width: 200,
+                height: 150,
+                marginTop: 10,
+                borderColor: "#E0E0E0",
+                borderWidth: 2,
+                borderRadius: 10,
+                padding: 20,
+                marginHorizontal: 10,
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 15,
+                  fontWeight: "bold",
+                  marginVertical: 7,
+                }}
+              >
+                15% Discounts
+              </Text>
+              <Text style={{ fontSize: 15, fontWeight: "500" }}>
+                Complete 5 stays to unlock level 2
+              </Text>
+            </Pressable>
+
+            <Pressable
+              style={{
+                width: 200,
+                height: 150,
+                marginTop: 10,
+                borderColor: "#E0E0E0",
+                borderWidth: 2,
+                borderRadius: 10,
+                padding: 20,
+                marginHorizontal: 20,
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 15,
+                  fontWeight: "bold",
+                  marginVertical: 7,
+                }}
+              >
+                10% Discounts
+              </Text>
+              <Text style={{ fontSize: 15, fontWeight: "500" }}>
+                Enjoy Discounts at participating at properties worldwide
+              </Text>
+            </Pressable>
+          </ScrollView>
+
           <Pressable
-            style={({ pressed }) => [
-              styles.pressable,
-              {
-                borderColor: pressed ? 'transparent' : '#FFC72C',
-                paddingHorizontal: 10, // Add horizontal padding
-                width: '90%', // Cover 90% of the width
-              },
-            ]}
-            onPress={() => {
-              // Handle rooms and guests press
+            style={{
+              marginTop: 40,
+              justifyContent: "center",
+              alignItems: "center",
             }}
           >
-            <Ionicons name="people" size={24} color="#007AFF" />
-            {/* You can add text or other content here */}
+            <Image
+              style={{ width: 200, height: 50, resizeMode: "cover" }}
+              source={{
+                uri: "https://assets.stickpng.com/thumbs/5a32a821cb9a85480a628f8f.png",
+              }}
+            />
           </Pressable>
-        </View>
-      </ScrollView>
-    </View>
+        </ScrollView>
+      </View>
+    </>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-  },
-  mainRectangle: {
-    backgroundColor: 'white',
-    flexDirection: 'column', // Change to column layout
-    alignItems: 'center',
-  },
-  pressable: {
-    borderColor: '#FFC72C',
-    borderWidth: 2,
-    paddingVertical: 15,
-    flexDirection: 'row',
-    alignItems: 'center',
-    flex: 1, // Make each Pressable cover equal width
-  },
-  notificationIcon: {
-    marginRight: 16, // Adjust the margin if needed
-    borderRadius: 50, // Make it round
-    padding: 8, // Add some padding for space
-  },
-});
-
 export default HomeScreen;
+
+const styles = StyleSheet.create({});
